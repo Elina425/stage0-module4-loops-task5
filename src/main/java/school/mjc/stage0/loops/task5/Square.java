9bfd8e994f3071b1a394ccb02690b336ffde3fb3
Elina <elina.melkonyan.y@tumo.org>
2022-08-13 23:15:59 +0400

    dd

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
+        for(int x=0; x<sideLength; x++){
+            System.out.println(" ");
+            for(int y=0; y<sideLength; y++){
+                System.out.print("8 ");
+            }
 
+        }
     }
 }

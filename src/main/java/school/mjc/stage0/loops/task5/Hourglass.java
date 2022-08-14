334d8905b5fe4733238eba7eef71a7534f6050c3
Elina <elina.melkonyan.y@tumo.org>
2022-08-15 01:26:38 +0400

    ppp

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,32 +3,50 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
 
-                for(int i=1; i<=height; i++)
-                {
-                    for(int k=1; k<=i-1; k++)
-                    {
-                        System.out.print(" ");
-                    }
-                    for(int j=1; j<=height-i+1; j++)
-                    {
-                        System.out.print("8");
-                    }
-                    System.out.println();
-                }
+        int i, j, k;
 
-                for(int i=4; i>=1; i--)
-                {
-                    for(int k=1; k<=i-1; k++)
-                    {
-                        System.out.print(" ");
-                    }
-                    for(int j=i; j<=height; j++)
-                    {
-                        System.out.print("8");
-                    }
-                    System.out.println();
-                }
+        // for loop for printing
+        // upper half
+        for (i = height; i >0; i--) {
+
+            // printing i spaces at
+            // the beginning of each row
+            for (j = height-i; j >=1; j--){
+                System.out.print(" ");
             }
+
+
+            // printing i to rows value
+            // at the end of each row
+            for (k = 1; k <= i; k++){
+                System.out.print("8 ");
+            }
+
+
+            System.out.println();
+        }
+
+        for (i = 1; i <=height; i++) {
+
+            // printing i spaces at
+            // the beginning of each row
+            for (j = 1; j <=height-i; j++){
+                System.out.print(" ");
+            }
+
+
+            // printing i to rows value
+            // at the end of each row
+            for (k = 1; k <= i; k++){
+                System.out.print("8 ");
+            }
+
+
+            System.out.println();
+        }
+
+
+    }
         }
 
 

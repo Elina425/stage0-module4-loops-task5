fef973a2b66604a2f343e8e855103c3f1a3974fb
Elina <elina.melkonyan.y@tumo.org>
2022-08-22 17:53:44 +0400

    gtgfuk

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -5,46 +5,31 @@ public class Hourglass {
 
         int i, j, k;
 
-        // for loop for printing
-        // upper half
-        for (i = height; i >0; i--) {
 
-            // printing i spaces at
-            // the beginning of each row
-            for (j = height-i; j >=1; j--){
+        for (i = height; i >1; i--) {
+
+
+            for (j = 1; j <=height-i; j++){
                 System.out.print(" ");
             }
 
-
-            // printing i to rows value
-            // at the end of each row
-            for (k = 1; k <= i; k++){
-                System.out.print("8 ");
+            for (k = 1; k <= 2*i-1; k++){
+                System.out.print("8");
             }
-
-
             System.out.println();
         }
 
         for (i = 1; i <=height; i++) {
 
-            // printing i spaces at
-            // the beginning of each row
             for (j = 1; j <=height-i; j++){
                 System.out.print(" ");
             }
 
-
-            // printing i to rows value
-            // at the end of each row
-            for (k = 1; k <= i; k++){
-                System.out.print("8 ");
+            for (k = 1; k <= 2*i-1; k++){
+                System.out.print("8");
             }
-
-
             System.out.println();
         }
-
 
     }
         }

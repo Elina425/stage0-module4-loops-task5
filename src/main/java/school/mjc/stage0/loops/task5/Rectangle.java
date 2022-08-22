2611cab9742520654ea52bc1a8ab792e5258dc78
Elina <elina.melkonyan.y@tumo.org>
2022-08-22 17:03:15 +0400

    kkkk

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -3,10 +3,11 @@ package school.mjc.stage0.loops.task5;
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
     for(int i=0; i<length; i++){
-    System.out.println(" ");
+
     for(int y=0; y<height; y++){
         System.out.print("8");
     }
+        System.out.println();
 }
     }
 }
